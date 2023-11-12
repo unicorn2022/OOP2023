dab5475c9a46fbf542bef61684091579cc26089a
unicorn2022 <496300118@qq.com>
2023-11-12 14:27:17 +0800

    Lab4 Diary

---
+++ VS_Solution/PersonDiary/x64/Debug/sample2.bat
@@ -0,0 +1,12 @@
+call pdadd.exe < sampleAdd.txt
+
+call pdshow.exe < sampleShow.txt
+
+call pdremove.exe < sampleRemove.txt
+
+pdlist
+
+pdlist 2022/4/10 2022/4/11
+
+pdlist 2025/4/10 2025/4/11
+pause
